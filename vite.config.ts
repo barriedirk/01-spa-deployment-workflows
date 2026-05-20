45b5a77ace2caab17c7273a02fc17086e3a69f24
Barrie Freyre <barriedirk@yahoo.com>
2026-05-19 23:07:34 -0500

    fix: set vite base path for local s3 subfolder routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/mi-bucket-frontend/", // Esto hace que Vite ponga /mi-bucket-frontend/ al inicio de todas las rutas
+});
